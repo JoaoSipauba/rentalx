1b1e61951f2d618b4c4073122123ba26a3d7f062
JoaoSipauba <joaosipauba@hotmail.com>
2022-04-24 13:18:33 -0300

    feat: creating method to create categories

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,32 @@
+import { v4 as uuidV4 } from "uuid";
+
+import { Category } from "../model/Category";
+
+// DTO => Data tranfer object
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+class CategoriesRepository {
+  private categories: Category[] = [];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  create({ name, description }: ICreateCategoryDTO): void {
+    const category = new Category();
+
+    Object.assign(category, {
+      id: uuidV4(),
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.categories.push(category);
+  }
+}
+
+export { CategoriesRepository };
--- src/routes/caregories.routes.ts
+++ src/routes/caregories.routes.ts
@@ -1,25 +1,14 @@
 import { Router } from "express";
-import { v4 as uuidV4 } from "uuid";
 
-import { Category } from "../model/Category";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
-
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category = new Category();
-
-  Object.assign(category, {
-    id: uuidV4(),
-    name,
-    description,
-    created_at: new Date(),
-  });
-
-  categories.push(category);
+  categoriesRepository.create({ name, description });
 
   return response.status(201).send();
 });
