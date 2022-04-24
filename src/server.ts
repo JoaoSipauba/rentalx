b2299984f3a3fd5bf75f33ee40c30ce594f7c459
JoaoSipauba <joaosipauba@hotmail.com>
2022-04-24 13:03:11 -0300

    feat: criando model de category

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/caregories.routes";
+
 const app = express();
+
+app.use(express.json());
+app.use("/categories", categoriesRoutes);
 
 app.listen(3333, () => console.log("Server is running!"));
