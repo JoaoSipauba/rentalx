d43e57937ad254899b96c52dacd07f41a1516b6f
JoaoSipauba <joaosipauba@hotmail.com>
2022-04-24 17:06:31 -0300

    feat: creating category useCase

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from "express";
 
 import { categoriesRoutes } from "./routes/categories.routes";
-import { specificationRoutes } from "./routes/specification.routes";
+import { specificationRoutes } from "./routes/specifications.routes";
 
 const app = express();
 
