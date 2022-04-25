cc0efa6add21fcf10ec183f36561561e0e252f24
JoaoSipauba <joaosipauba@hotmail.com>
2022-04-25 17:19:06 -0300

    feat: saving import data on categories array

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,13 +2,14 @@ import { Router } from "express";
 import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { importCategoryController } from "../modules/cars/useCases/importCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
 
 const upload = multer({
-  dest: "./tmp"
-})
+  dest: "./tmp",
+});
 
 categoriesRoutes.post("/", (request, response) => {
   return createCategoryController.handle(request, response);
@@ -19,9 +20,7 @@ categoriesRoutes.get("/", (request, response) => {
 });
 
 categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
-  const { file } = request;
-  console.log(file);
-  return response.send();
+  return importCategoryController.handle(request, response);
 });
 
 export { categoriesRoutes };
