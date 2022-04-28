daf97a68d60d326033429fce3b4771a3b483a19e
JoaoSipauba <joaosipauba@hotmail.com>
2022-04-28 20:16:50 -0300

    feat: creating authenticated routes

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -10,6 +10,7 @@ class UsersRepository implements IUsersRepository {
   constructor() {
     this.repository = getRepository(User);
   }
+
   async create({
     name,
     email,
@@ -28,6 +29,12 @@ class UsersRepository implements IUsersRepository {
 
   async findByEmail(email: string): Promise<User | undefined> {
     const user = await this.repository.findOne({ email });
+
+    return user;
+  }
+
+  async findById(id: string): Promise<User | undefined> {
+    const user = await this.repository.findOne({ id });
 
     return user;
   }
