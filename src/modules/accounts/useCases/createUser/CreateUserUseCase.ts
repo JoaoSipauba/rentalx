dad3c0c4f09da779160153dec4f0e07d6e52b045
JoaoSipauba <joaosipauba@hotmail.com>
2022-04-28 17:00:55 -0300

    feat: creating user controller and updating userRepository

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,8 +1,9 @@
-import { inject } from "tsyringe";
+import { inject, injectable } from "tsyringe";
 
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
+@injectable()
 class CreateUserUseCase {
   constructor(
     @inject("UsersRepository")
@@ -11,14 +12,12 @@ class CreateUserUseCase {
 
   async execute({
     name,
-    username,
     email,
     driver_license,
     password,
   }: ICreateUserDTO): Promise<void> {
     await this.usersRepository.create({
       name,
-      username,
       email,
       driver_license,
       password,
